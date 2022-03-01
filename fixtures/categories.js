e5f0f00b8ca221c2c116c83a870157c719871f10
Nathan Flacher <nathan.flacher.pro@gmail.com>
2022-03-01 02:18:28 +0100

    beta of the categories fixtures

---
+++ fixtures/categories.js
@@ -0,0 +1,11 @@
+const { ObjectID: ObjectId } = require('mongodb');
+
+module.exports = [
+    { _id: ObjectId(), title:'CV' },
+    { _id: ObjectId(), title:'Qui suis-je' },
+    { _id: ObjectId(), title:'Compétences' },
+    { _id: ObjectId(), title:'Réalisations' },
+    { _id: ObjectId(), title:'Services' },
+    { _id: ObjectId(), title:'Blog' },
+    { _id: ObjectId(), title:'Contact' },
+]
